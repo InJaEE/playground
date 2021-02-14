768c51fc1a9b2720190abdb5c0480fc4b0c02e95
InJaEE <in11202@naver.com>
2021-02-14 12:48:25 +0900

    add winston, morgan

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { config } from '@/config/index';
 import router from '@/router/index';
 import prisma from '@/database';
+import logger from '@/utils/logger';
+import morgan from 'morgan';
 import schedule from '@/schedule/rank';
 import cors from 'cors';
 
@@ -9,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
 app.use(
 	cors({
 		origin: 'http://localhost:3000',
@@ -18,7 +21,12 @@ app.use(
 app.use('/api', router);
 
 prisma.$connect().then(() => {
-	console.info('Prisma connected!');
+	logger.info('Prisma connected!');
+	try {
+		throw new Error('Test Error!');
+	} catch (error) {
+		logger.error(error);
+	}
 	schedule();
 	app.listen(config.port, () => {
 		console.info(`\u001b[96mServer on port at\u001b[00m \u001b[93m${config.port}\u001b[00m`);
---
+++ server/src/utils/logger.ts
@@ -0,0 +1,63 @@
+import winston from 'winston';
+import winstonDaily from 'winston-daily-rotate-file';
+import appRoot from 'app-root-path';
+
+const { combine, timestamp, printf, label } = winston.format;
+
+const logFormat = printf(({ level, message, label, timestamp }) => {
+	return `${timestamp} [${label}] ${level}: ${message}`;
+});
+const dirname = `${appRoot}\\logs`;
+const datePattern = 'YYYY-MM-DD';
+
+// error: 0, warn: 1, info: 2, http: 3, verbose: 4, debug: 5, silly: 6
+const logger = winston.createLogger({
+	format: combine(
+		label({
+			label: 'Playground',
+		}),
+		timestamp({
+			format: 'YYYY-MM-DD HH:mm:ss',
+		}),
+		logFormat,
+	),
+	transports: [
+		new winstonDaily({
+			level: 'info',
+			datePattern,
+			dirname,
+			filename: `%DATE%.log`,
+			maxFiles: 30,
+			zippedArchive: true,
+		}),
+		new winstonDaily({
+			level: 'error',
+			datePattern,
+			dirname,
+			filename: `%DATE%.error.log`,
+			maxFiles: 30,
+			zippedArchive: true,
+		}),
+	],
+	exceptionHandlers: [
+		// uncaught exception
+		new winstonDaily({
+			level: 'error',
+			datePattern,
+			dirname,
+			filename: `%DATE%.exception.log`,
+			maxFiles: 30,
+			zippedArchive: true,
+		}),
+	],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+	logger.add(
+		new winston.transports.Console({
+			format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
+		}),
+	);
+}
+
+export default logger;
